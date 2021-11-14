7913e381790ce03c9ae82eabdf37487b28b67fb2
QuangVinh <vinhdq67@gmail.com>
2021-11-14 23:49:27 +0700

    create validate function, show hide pwd button

--- loginform/js/main.js
+++ loginform/js/main.js
@@ -2,7 +2,10 @@ const logUser = document.querySelector('#name')
 const logPass = document.querySelector('#pass')
 const regUser = document.querySelector('#regname')
 const regPass = document.querySelector('#regpass')
-const regRePass = document.querySelector('#regrepass')
+const regRePass = document.querySelector('#reregpass')
+const noti = document.querySelector('.noti');
+let notiContent = "";
+let err = [];
 
 function focusFunction(x) {
     let element = document.querySelector(`label[for=${x}]`);
@@ -20,11 +23,105 @@ function blurFunction(x) {
 }
 
 function registerFunction() {
-    let noti = document.querySelector('.noti');
-    let inputs = document.querySelectorAll('input')
-    for (let item of inputs) {
-        item.value = "";
+    let style = 'color:red; position:absolute; top:102%; font-size:12px';
+    let inputs = document.querySelectorAll("input");
+    let afterElements = document.querySelectorAll(".input p");
+    for (item of afterElements) {
+        item.remove();
     }
-    noti.style.display = "block";
-    setTimeout(function () { noti.style.display = "none"; }, 2000)
+    if (validateRegister()) {
+        for (let item of inputs) {
+            item.value = "";
+            blurFunction('regname');
+            blurFunction('regpass');
+            blurFunction('reregpass');
+        }
+        noti.style.opacity = "100";
+        setTimeout(function () {
+            noti.style.opacity = "0";
+        }, 1500);
+    }
+    else {
+        for (let item of err) {
+            if (item == 0) {
+                regUser.after(document.createElement('p'));
+                regUser.parentElement.querySelector("p").style = style;
+                regUser.parentElement.querySelector("p").innerHTML = 'Username cannot be empty';
+            }
+            if (item == 1) {
+                regPass.after(document.createElement('p'));
+                regPass.parentElement.querySelector("p").style = style;
+                regPass.parentElement.querySelector("p").innerHTML = 'Password must be at least 6 characters and not exceed 32 characters';
+            }
+            if (item == 2) {
+                regRePass.after(document.createElement('p'));
+                regRePass.parentElement.querySelector("p").style = style;
+                regRePass.parentElement.querySelector("p").innerHTML = 'Confirmed password is not match';
+            }
+        }
+    }
+}
+
+function loginFunction() {
+    if (validateLogin()) {
+        noti.querySelector('.notibox').innerHTML = `<p style = "color:green"><i class="fas fa-check-circle"></i>&nbsp;Login Successful</p>`;
+    } else {
+        noti.querySelector('.notibox').innerHTML = notiContent;
+    }
+    for (item of err) {
+        if (item == 0) {
+            logUser.parentElement.querySelector('label').style.color = 'red';
+        }
+        if (item == 1) {
+            logPass.parentElement.querySelector('label').style.color = 'red';
+        }
+    }
+    noti.style.opacity = "100";
+    setTimeout(function () { noti.style.opacity = "0"; }, 1500)
+}
+
+function validateLogin() {
+    notiContent = "";
+    err = [];
+    if (logUser.value == "") {
+        notiContent += `<p style = "color:red"><i class="fas fa-times-circle"></i>&nbsp;Username cannot be empty</p>`;
+        err.push(0);
+    }
+    if (logPass.value == '') {
+        notiContent += `<p style = "color:red"><i class="fas fa-times-circle"></i>&nbsp;Password cannot be empty</p>`;
+        err.push(1)
+    }
+    if (notiContent != '') {
+        return false;
+    }
+    return true;
+}
+
+function validateRegister() {
+    err = [];
+    if (regUser.value == "") {
+        err.push(0);
+    }
+    if (regPass.value.length < 6 || regPass.value.length > 32) {
+        err.push(1)
+    }
+    if (regRePass.value != regPass.value || regRePass.value == '') {
+        err.push(2)
+    }
+    if (err.length > 0) {
+        return false;
+    }
+    return true;
+}
+
+function showHidePassword(){
+    let showBtn = document.querySelector('.input .showPwd');
+    let inputPwd = document.querySelector('.input #pass');
+    if (inputPwd.type == 'password'){
+        inputPwd.type = 'text';
+        showBtn.innerHTML = `<i class="far fa-eye" onclick="showHidePassword()"></i>`
+    } else {
+        inputPwd.type = 'password';
+        showBtn.innerHTML = `<i class="far fa-eye-slash" onclick="showHidePassword()"></i>`
+    }
 }
